4735f406ca41a9fe28de047568199ad0e2952443
quadspell <quade-s@sbcglobal.net>
2015-03-11 15:54:19 -0500

    ending graph fix.0

--- music.c
+++ music.c
@@ -5,13 +5,15 @@
 
 #define SAMPLE_RATE 44100.0 //read from file samples per second
 #define DISPLAY_RATE 10.0
-#define DISPLAY_MOD 30.0
+#define DISPLAY_MOD 50.0
 
 void importFreq(double raw[], double refine[], int songLength);//FINISH_ME
-void playDisplay(double refine[]);
+void playDisplay(double refine[], int songLength);
 double minFreq(double array[]);
 double maxFreq(double array[]);
 double avgFreq(double array[]);
+
+void refineArray(double array[]);
 
 int main(void)
 {
@@ -26,11 +28,11 @@ int main(void)
 
     double refineFreq[songLength];
 
-    double testRefineFreq[songLength];
-
     importFreq(rawFreq, refineFreq, songLength);
 
-    playDisplay(refineFreq);
+    refineArray(refineFreq);
+
+    playDisplay(refineFreq, songLength);
 
 
     return 0;
@@ -48,7 +50,6 @@ void importFreq(double raw[], double refine[], int songLength){//FINISH_ME
     for(i=0;i< songLength;i++){
         for(j=0;j<SAMPLE_RATE/DISPLAY_RATE;j++){//importing sound
             //raw[j] = 2.0*(double)rand()/(double)RAND_MAX - 1;//test
-
             scanf("%lf\n", &raw[j]);//real import
 
             rawImport += fabs(raw[j]);
@@ -56,24 +57,26 @@ void importFreq(double raw[], double refine[], int songLength){//FINISH_ME
 
         refine[i] = ((rawImport / (SAMPLE_RATE/DISPLAY_RATE)) * DISPLAY_MOD);//change to be percent based off of the average value of the song.
 
-        //find min, max, and average.
+        //find min, max, and average
 
         rawImport =0.0;
     }
     printf("Frequency imported\n");//test
 
+
+
 }
 
-void playDisplay(double refine[]){
+void playDisplay(double refine[], int songLength){
     int i = 0, j = 0;
     double t = clock()/((double)CLOCKS_PER_SEC);
-    while(refine[i]!= '\0'){
-        if(t <= clock()/((double)CLOCKS_PER_SEC) - .1){
+    for(i=0;i<songLength;i++){
+        if(t <= clock()/((double)CLOCKS_PER_SEC) - 1/DISPLAY_RATE){
             for(j=0;j<refine[i];j++){
                 printf("*");
             }
             printf("\n");
-            i++;
+
             t = clock()/((double)CLOCKS_PER_SEC);
         }
     }
@@ -106,4 +109,8 @@ double avgFreq(double array[]){
     }
     avg = avg/i;
     return avg;
+}
+
+void refineArray(double array[]){
+    //minFreq();
 }
